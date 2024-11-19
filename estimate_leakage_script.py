011cb4eb8fed05b106a7f05755910d1d5f94350d
“MMakonnen” <mikael.makonnen@gmail.com>
2024-11-19 17:18:27 +0100

    current working version of complete leakage estimation script

--- estimate_leakage_script.py
+++ estimate_leakage_script.py
@@ -1,5 +1,6 @@
 from synthetic_dataset import generate_synthetic_data_leakage
 from sklearn.model_selection import train_test_split
+import torch
 
 seed = 42
 
@@ -27,3 +28,19 @@ X_train, X_val, c_train, c_val, hat_c_train, hat_c_val, y_train, y_val = train_t
     X_temp, c_temp, hat_c_temp, y_temp, test_size=0.25, random_state=42
 )
 # Now, training data is 60%, validation (calibration) data is 20%, test data is 20%
+
+
+# Convert data to PyTorch tensors (and move on device for accelerated computations)
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
+
+c_train_tensor = torch.FloatTensor(c_train).to(device)
+c_val_tensor = torch.FloatTensor(c_val).to(device)
+c_test_tensor = torch.FloatTensor(c_test).to(device)
+
+c_hat_train_tensor = torch.FloatTensor(c_hat_train).to(device)
+c_hat_val_tensor = torch.FloatTensor(c_hat_val).to(device)
+c_hat_test_tensor = torch.FloatTensor(c_hat_test).to(device)
+
+y_train_tensor = torch.LongTensor(y_train).to(device)
+y_val_tensor = torch.LongTensor(y_val).to(device)
+y_test_tensor = torch.LongTensor(y_test).to(device)
