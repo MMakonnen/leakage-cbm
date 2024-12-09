b18bcc645b8c5f7704543f2f68ed67eb899d7293
“MMakonnen” <mikael.makonnen@gmail.com>
2024-12-09 19:44:40 +0100

    updated data preparation

--- data/data_prep.py
+++ data/data_prep.py
@@ -1,17 +1,15 @@
+from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import TensorDataset, DataLoader
-from sklearn.model_selection import train_test_split
 
 
-# Function creating data splits in desired format
+# ...
 def split_data(X, c, c_hat, y_zero_based, train_size, val_size, test_size, seed):
-    # First, split off the test set
     X_temp, X_test, c_temp, c_test, c_hat_temp, c_hat_test, y_temp, y_test = train_test_split(
         X, c, c_hat, y_zero_based, test_size=test_size, random_state=seed
     )
 
-    # Then, split the remaining data into training and validation sets
-    val_ratio = val_size / (train_size + val_size)  # Adjust validation size accordingly
+    val_ratio = val_size / (train_size + val_size)
     (
         X_train,
         X_val,
@@ -22,14 +20,8 @@ def split_data(X, c, c_hat, y_zero_based, train_size, val_size, test_size, seed)
         y_train,
         y_val,
     ) = train_test_split(
-        X_temp,
-        c_temp,
-        c_hat_temp,
-        y_temp,
-        test_size=val_ratio,
-        random_state=seed,
+        X_temp, c_temp, c_hat_temp, y_temp, test_size=val_ratio, random_state=seed
     )
-    # Now, training, validation, and test sets are split according to specified sizes
     return (
         X_train,
         X_val,
@@ -61,7 +53,6 @@ def prepare_data_loaders(
     seed,
     device,
 ):
-    # Convert data to PyTorch tensors and move to the specified device
     c_train_tensor = torch.FloatTensor(c_train).to(device)
     c_val_tensor = torch.FloatTensor(c_val).to(device)
     c_test_tensor = torch.FloatTensor(c_test).to(device)
@@ -74,11 +65,9 @@ def prepare_data_loaders(
     y_val_tensor = torch.LongTensor(y_val).to(device)
     y_test_tensor = torch.LongTensor(y_test).to(device)
 
-    # Create DataLoader for training, validation, and testing
     data_loader_seed = torch.Generator(device=device)
     data_loader_seed.manual_seed(seed)
 
-    # Dataset and DataLoader for g_b (predicting y from c)
     train_dataset_gb = TensorDataset(c_train_tensor, y_train_tensor)
     val_dataset_gb = TensorDataset(c_val_tensor, y_val_tensor)
     test_dataset_gb = TensorDataset(c_test_tensor, y_test_tensor)
@@ -92,8 +81,6 @@ def prepare_data_loaders(
     val_loader_gb = DataLoader(val_dataset_gb, batch_size=batch_size, shuffle=False)
     test_loader_gb = DataLoader(test_dataset_gb, batch_size=batch_size, shuffle=False)
 
-    # Dataset and DataLoader for g_a (predicting y from c_hat and c)
-    # Concatenate c_hat and c
     c_hat_c_train_tensor = torch.cat((c_hat_train_tensor, c_train_tensor), dim=1)
     c_hat_c_val_tensor = torch.cat((c_hat_val_tensor, c_val_tensor), dim=1)
     c_hat_c_test_tensor = torch.cat((c_hat_test_tensor, c_test_tensor), dim=1)
